841292bc7d84ae33d209e3c30d31861cfc6fb65c
logicalforhad <forhad.univdhaka@gmail.com>
2015-08-10 23:19:07 +0600

    bootstrap added and ui router added

--- springtest/src/main/resources/static/js/app/config/config.js
+++ springtest/src/main/resources/static/js/app/config/config.js
@@ -1,13 +1,25 @@
 (function () {
     "use strict";
     function config($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise("/");
+        $urlRouterProvider.otherwise("/profile");
         $stateProvider
             .state("/", {
                 url: "/",
                 templateUrl: "js/app/views/home.html",
                 controller: "homeController",
                 controllerAs: 'vm'
+            })
+            .state("/.profile",{
+                url:"profile",
+                templateUrl:"js/app/views/profile.html"
+            })
+            .state("/.presentation",{
+                url:"presentation",
+                "templateUrl":"js/app/views/presentation.html"
+            })
+            .state("/.summary",{
+                url:"summary",
+                "templateUrl":"js/app/views/summary.html"
             });
     }
     angular.module("mlevel").config(config);
--- springtest/src/main/resources/static/js/app/controllers/homeController.js
+++ springtest/src/main/resources/static/js/app/controllers/homeController.js
@@ -2,23 +2,6 @@
     "use strict";
     function homeController() {
         var vm=this;
-        vm.step=1;
-        vm.previousStep=previousStep;
-        vm.nextStep=nextStep;
-        vm.save=save;
-        vm.speechEdit=speechEdit;
-        function previousStep(){
-            vm.step--;
-        }
-        function nextStep(){
-            vm.step++;
-        }
-        function save(model) {
-            console.log(model);
-        }
-        function speechEdit(){
-            vm.step=2;
-        }
     }
     angular.module("mlevel").controller("homeController", homeController);
 }());
